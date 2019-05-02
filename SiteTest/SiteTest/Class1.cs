ee3d8f7dceeeaa1594c0ed411be0cb1a23d02643
Maxim-Veretin <maxim.veretin.2410@gmail.com>
2019-05-02 11:26:25 +0700

    find +

--- SiteTest/SiteTest/Class1.cs
+++ SiteTest/SiteTest/Class1.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
 
 //План тестирования:
 //1. Проверить заголовок страницы
@@ -25,29 +26,52 @@ namespace SiteTest
         public void MainTitle()
         {
             webDriver.Url = "https://www.nstu.ru/";
+
             Assert.AreEqual("НГТУ. Новосибирский государственный технический университет", webDriver.Title);
 
-            //webDriver.Close();
+            webDriver.Close();
         }
 
         [TestCase]//видимость объектов
         public void DisplayObject()
         {
             webDriver.Url = "https://www.nstu.ru/";
+
             IWebElement element = webDriver.FindElement(By.XPath("//*[@id=\"query\"]"));
             bool status = element.Displayed;
 
-            //webDriver.Close();
+            webDriver.Close();
         }
 
         [TestCase]//переход по ссылке
         public void LinkPath()
         {
             webDriver.Url = "https://www.nstu.ru/";
+
             webDriver.Navigate().Forward();
+            webDriver.Navigate().Back();
+
+           // webDriver.Close();
         }
 
         [TestCase]//заполнение текстового поля
+        public void Enter()
+        {
+            webDriver.Url = "https://sibsutis.ru/";
+            
+
+            IWebElement search = webDriver.FindElement(By.XPath("//*[@id=\"qqq\"]"));//ссылка на строку поиска
+            search.SendKeys("]t]аогевгоеошгеогеокык7говер");//ввод запроса
+            
+            IWebElement button = webDriver.FindElement(By.XPath("//*[@id=\"layout\"]/div[2]/div[2]/div[2]/div[3]/form/input[2]"));//ссылка на кнопку найти
+            button.Click();
+
+            Assert.IsTrue(webDriver.PageSource.Contains("К сожалению, на ваш поисковый запрос ничего не найдено."));
+
+            webDriver.Close();
+        }
+
+        [TestCase]//эмуляция нажатия на кнопку
 
         [TearDown]//все тесты выполнены
         public void TestEnd()
